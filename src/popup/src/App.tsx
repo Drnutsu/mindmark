bd6572b68331b9d00c8747a2b8353bea3509c774
Nutsu <dentistnut@gmail.com>
2020-10-23 23:58:31 +0700

    added react flow as deps.

--- src/popup/src/App.tsx
+++ src/popup/src/App.tsx
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import ReactFlow from 'react-flow-renderer'
+
+import logo from './logo.svg'
+import './App.css'
+
+const elements = [
+  {
+    id: '1',
+    type: 'input', // input node
+    data: { label: 'Input Node' },
+    position: { x: 250, y: 25 }
+  },
+  // default node
+  {
+    id: '2',
+    // you can also pass a React component as a label
+    data: { label: <div>Default Node</div> },
+    position: { x: 100, y: 125 }
+  },
+  {
+    id: '3',
+    type: 'output', // output node
+    data: { label: 'Output Node' },
+    position: { x: 250, y: 250 }
+  },
+  // animated edge
+  { id: 'e1-2', source: '1', target: '2', animated: true },
+  { id: 'e2-3', source: '2', target: '3' }
+]
+
+const openWindow = () => {
+  chrome.windows.create(
+    {
+      url: 'https://www.google.com',
+      type: 'popup'
+    },
+    (win) => {
+      console.log(' win : ', win)
+    }
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div style={{ height: 300 }}>
+        <ReactFlow elements={elements} />
+      </div>
+      <button onClick={openWindow}>OPEN WINDOW</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
